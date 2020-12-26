c8ce355fd4d05cfb1ef19ea0c72bd1acddf9d296
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2020-12-26 16:11:36 +0100

    .

--- homophonic_v2.py
+++ homophonic_v2.py
@@ -24,15 +24,15 @@ import cProfile
 import pstats
 
 PROFILE = False
-DEBUG = False
+DEBUG = True
 
-# plain always lower case
-ALPHABET = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+RECURSION_LIMIT = 1500  # default is 1000
 
-
+ALPHABET = [chr(i) for i in range(ord('a'), ord('z') + 1)]  # plain always lower case
 EPSILON_NO_OCCURENCES = 1e-99  # zero has - infinite as log, must be << 1
+EPSILON_DELTA_FLOAT = 0.000001  # to compare floats
+EPSILON_PROBA = 1 / 1000  # to make sure we can give up searching
 
-RECURSION_LIMIT = 1500 # default is 1000
 
 class Ngrams:
     """ Ngrams : says the frequency of N grams (log (occurences / sum all) """
@@ -61,11 +61,11 @@ class Ngrams:
         sum_occurences = sum(raw_frequency_table.values())
 
         # for normal values
-        self._content = {q: math.log10(raw_frequency_table[q] / sum_occurences) for q in raw_frequency_table}
+        self._log_freq_table = {q: math.log10(raw_frequency_table[q] / sum_occurences) for q in raw_frequency_table}
 
         # complete for absent values
         def_log_value = math.log10(EPSILON_NO_OCCURENCES / sum_occurences)
-        self._content.update({''.join(letters): def_log_value for letters in itertools.product(ALPHABET, repeat=self._size) if ''.join(letters) not in self._content})
+        self._log_freq_table.update({''.join(letters): def_log_value for letters in itertools.product(ALPHABET, repeat=self._size) if ''.join(letters) not in self._log_freq_table})
 
         after = time.time()
         elapsed = after - before
@@ -76,9 +76,14 @@ class Ngrams:
         """ property """
         return self._size
 
+    @property
+    def log_freq_table(self) -> typing.Dict[str, float]:
+        """ property """
+        return self._log_freq_table
+
     def __str__(self) -> str:
         """ for debug """
-        return pprint.pformat(self._content)
+        return pprint.pformat(self._log_freq_table)
 
 
 NGRAMS: typing.Optional[Ngrams] = None
@@ -126,18 +131,17 @@ class Dictionary:
         elapsed = after - before
         print(f"Word list file '{filename}' loaded in {elapsed:2.2f} seconds")
 
-
-    def detected_words(self, plain: str) -> typing.List[str]:
+    def extracted_words(self, plain: str) -> typing.Tuple[float, typing.List[str]]:
         """ Tells the  of (more or less) plain text from the dictionary  """
 
-        def words_probability(words: typing.Tuple[str]) -> float:
+        def words_probability(words: typing.List[str]) -> float:
             """ Quality of a list of word (probability) """
             return sum(map(lambda w: self._log_frequency_table.get(w, self._worst_frequency), words))
 
         @functools.lru_cache(maxsize=None)
         def splits(text: str) -> typing.List[typing.Tuple[str, str]]:
             """ All ways to split some text into a first word and remainder """
-            return [(text[:cut+1], text[cut+1:]) for cut in range(min(self._longest_word, len(text)))]
+            return [(text[:cut + 1], text[cut + 1:]) for cut in range(min(self._longest_word, len(text)))]
 
         @functools.lru_cache(maxsize=None)
         def segment_rec(text: str) -> typing.List[str]:
@@ -149,8 +153,8 @@ class Dictionary:
             candidates = [[first] + segment_rec(rest) for first, rest in splits(text)]
             return max(candidates, key=words_probability)
 
-        return segment_rec(plain)
-
+        best_words = segment_rec(plain)
+        return words_probability(best_words), best_words
 
     def __str__(self) -> str:
         """ for debug """
@@ -181,15 +185,37 @@ class Cipher:
                                 continue
                         self._content.append(code)
 
-        # the different codes in cipher
-        self._cipher_codes = list(set(self._content))
-
-        # a table where how many times ngram appears
+        # the cipher as it appears
         self._cipher_str = ''.join(self._content)
-        self._quadgram_number_occurence_table = collections.Counter([self._cipher_str[p: p + NGRAMS.size] for p in range(len(self._cipher_str) - (NGRAMS.size - 1))])
+
+        # the different codes in cipher
+        self._cipher_codes = ''.join(set(self._content))
+
+        # list of cipher quadgrams with duplications
+        cipher_quadgrams = [self._cipher_str[p: p + NGRAMS.size] for p in range(len(self._cipher_str) - (NGRAMS.size - 1))]
+
+        # set of all quadgrams in cipher
+        self._quadgrams_set = set(cipher_quadgrams)
+
+        # a table where how many times quadgrams appear in cipher
+        self._quadgrams_number_occurence_table = collections.Counter(cipher_quadgrams)
+
+        # a table from a cipher to quadgrams that contain it
+        self._quadgrams_localization_table: typing.Dict[str, typing.List[str]] = collections.defaultdict(list)
+        for quadgram in self._quadgrams_set:
+            for code in quadgram:
+                self._quadgrams_localization_table[code].append(quadgram)
+
+    def show_plain(self, selected_words: typing.List[str]) -> None:
+        """ need to be here because this objects know jow many characters are missing """
+        cipher_length = len(self._cipher_str)
+        plain_length = sum(map(len, selected_words))
+        coverage = plain_length / cipher_length * 100
+        print(f"Covers {coverage:.2f}%")
+        print(' '.join(selected_words))
 
     @property
-    def cipher_codes(self) -> typing.List[str]:
+    def cipher_codes(self) -> str:
         """ property """
         return self._cipher_codes
 
@@ -199,9 +225,19 @@ class Cipher:
         return self._cipher_str
 
     @property
-    def quadgram_number_occurence_table(self) -> typing.Counter[str]:
+    def quadgrams_set(self) -> typing.Set[str]:
         """ property """
-        return self._quadgram_number_occurence_table
+        return self._quadgrams_set
+
+    @property
+    def quadgrams_number_occurence_table(self) -> typing.Counter[str]:
+        """ property """
+        return self._quadgrams_number_occurence_table
+
+    @property
+    def quadgrams_localization_table(self) -> typing.Dict[str, typing.List[str]]:
+        """ property """
+        return self._quadgrams_localization_table
 
     def __str__(self) -> str:
         return self._cipher_str
@@ -213,29 +249,170 @@ CIPHER: typing.Optional[Cipher] = None
 class Decrypter:
     """ A decrypter : basically a dictionary cipher -> plain """
 
-    def __init__(self, cipher: Cipher) -> None:
-        self._cipher = cipher
-        self._table = {c: '' for c in cipher.cipher_codes}
+    def __init__(self) -> None:
+        assert CIPHER is not None
+        self._table = {c: '' for c in CIPHER.cipher_codes}
 
     def install(self, allocation: typing.Dict[str, str]) -> None:
         """ install an initial table """
         for cipher, plain in allocation.items():
             self._table[cipher] = plain
 
-    def decode_one(self, cipher: str) -> str:
+    def decode_some(self, cipher_part: str) -> str:
         """ decode """
-        assert cipher in self._table
-        return self._table[cipher]
+        return ''.join(map(lambda c: self._table[c], cipher_part))
 
     def apply(self) -> str:
         """ apply the table (get a new plain from a cipher) """
-        return ''.join([self.decode_one(c) for c in self._cipher.cipher_str])
+        assert CIPHER is not None
+        return self.decode_some(CIPHER.cipher_str)
+
+    def swap(self, cipher1: str, cipher2: str) -> None:
+        """ swap """
+        self._table[cipher1], self._table[cipher2] = self._table[cipher2], self._table[cipher1]
 
     def __str__(self) -> str:
         return pprint.pformat(self._table)
 
 
 DECRYPTER: typing.Optional[Decrypter] = None
+
+
+class Attacker:
+    """ Attacker """
+
+    def __init__(self) -> None:
+
+        assert DICTIONARY is not None
+        assert CIPHER is not None
+        assert DECRYPTER is not None
+        assert NGRAMS is not None
+
+        # make initial random allocation
+        allocation = {k: random.choice(ALPHABET) for k in CIPHER.cipher_codes}
+
+        # put it in crypter
+        DECRYPTER.install(allocation)
+
+        # a table for remembering frequencies
+        self._quadgrams_frequency_quality_table: typing.Dict[str, float] = dict()
+
+        # quadgram frequency quality table
+        for quadgram in CIPHER.quadgrams_set:
+
+            # plain
+            plain = DECRYPTER.decode_some(quadgram)
+
+            # remembered
+            self._quadgrams_frequency_quality_table[quadgram] = NGRAMS.log_freq_table[plain] * CIPHER.quadgrams_number_occurence_table[quadgram]
+
+        # quadgram overall frequency quality of cipher
+        # summed
+        self._overall_quadgrams_frequency_quality = sum(self._quadgrams_frequency_quality_table.values())
+
+        if DEBUG:
+            self._check_quadgram_frequency_quality()
+
+    def _check_quadgram_frequency_quality(self) -> None:
+        """ Evaluates quality from quadgram frequency DEBUG """
+
+        assert DEBUG
+
+        assert DECRYPTER is not None
+        assert NGRAMS is not None
+
+        # debug check
+        qcheck = 0.
+        plain = DECRYPTER.apply()
+        for position in range(len(plain) - NGRAMS.size + 1):
+            quadgram = plain[position: position + NGRAMS.size]
+            qcheck += NGRAMS.log_freq_table[quadgram]
+
+        assert abs(qcheck - self._overall_quadgrams_frequency_quality) < EPSILON_DELTA_FLOAT
+
+    def _swap(self, cipher1: str, cipher2: str) -> None:
+        """ swap """
+
+        assert NGRAMS is not None
+        assert CIPHER is not None
+        assert DECRYPTER is not None
+
+        DECRYPTER.swap(cipher1, cipher2)
+
+        # effect
+
+        for cipher in cipher1, cipher2:
+            for quadgram in CIPHER.quadgrams_localization_table[cipher]:
+
+                # value obliterated
+                self._overall_quadgrams_frequency_quality -= self._quadgrams_frequency_quality_table[quadgram]
+
+                # new plain
+                plain = DECRYPTER.decode_some(quadgram)
+
+                # new value
+                new_value = NGRAMS.log_freq_table[plain] * CIPHER.quadgrams_number_occurence_table[quadgram]
+
+                # remembered
+                self._quadgrams_frequency_quality_table[quadgram] = new_value
+
+                # summed
+                self._overall_quadgrams_frequency_quality += new_value
+
+    def _climb(self) -> bool:
+        """ climb : try to improve things... """
+
+        assert CIPHER is not None
+        assert DECRYPTER is not None
+
+        changes = [(cipher1, cipher2) for cipher1, cipher2 in itertools.combinations(CIPHER.cipher_codes, 2) if DECRYPTER.decode_some(cipher1) != DECRYPTER.decode_some(cipher2)]
+        random.shuffle(changes)
+
+        for cipher1, cipher2 in changes:
+
+            # -----------------------
+            #  does the change improve things ?
+            # -----------------------
+
+            # keep a note of quality before change
+            old_overall_quadgrams_frequency_quality = self._overall_quadgrams_frequency_quality
+
+            # apply change now
+            self._swap(cipher1, cipher2)
+
+            if DEBUG:
+                self._check_quadgram_frequency_quality()
+
+            # did the quality improve ?
+            if self._overall_quadgrams_frequency_quality > old_overall_quadgrams_frequency_quality:
+                # yes : stop looping : we have improved
+                return True
+
+            # no improvement so undo
+            self._swap(cipher1, cipher2)
+
+            if DEBUG:
+                self._check_quadgram_frequency_quality()
+
+            # restore value
+            self._overall_quadgrams_frequency_quality = old_overall_quadgrams_frequency_quality
+
+        return False
+
+    def ascend(self) -> None:
+        """ ascend : keep climbing until fails to do so """
+
+        while True:
+
+            # keeps climbing until fails to do so
+            succeeded = self._climb()
+            if not succeeded:
+                print()
+                return
+            print("/", end='', flush=True)
+
+
+ATTACKER: typing.Optional[Attacker] = None
 
 
 def main() -> None:
@@ -270,26 +447,42 @@ def main() -> None:
     debug_mode = args.debug
 
     cipher_file = args.cipher
-    cipher = Cipher(cipher_file, debug_mode)
-    print(f"Cipher='{cipher}'")
+    global CIPHER
+    CIPHER = Cipher(cipher_file, debug_mode)
+    print(f"Cipher='{CIPHER}'")
 
     global DECRYPTER
-    DECRYPTER = Decrypter(cipher)
+    DECRYPTER = Decrypter()
 
     if debug_mode:
-        identity = {c:c for c in cipher.cipher_codes}
+        identity = {c: c for c in CIPHER.cipher_codes}
         DECRYPTER.install(identity)
         plain = DECRYPTER.apply()
-        detected_words = DICTIONARY.detected_words(plain)
-        print(f"{detected_words=}")
+        dictionary_quality, selected_words = DICTIONARY.extracted_words(plain)
+        print(f"{dictionary_quality=}")
+        CIPHER.show_plain(selected_words)
         return
 
-    # attacker : TODO
+    best_dictionary_quality_sofar: typing.Optional[float] = None
+    while True:
+
+        # start a new session
+        global ATTACKER
+        ATTACKER = Attacker()
+        ATTACKER.ascend()
+
+        # get dictionary quality of result
+        clear = DECRYPTER.apply()
+        dictionary_quality, selected_words = DICTIONARY.extracted_words(clear)
+
+        if best_dictionary_quality_sofar is None or dictionary_quality > best_dictionary_quality_sofar:
+            print(f"{dictionary_quality=}")
+            CIPHER.show_plain(selected_words)
+            best_dictionary_quality_sofar = dictionary_quality
 
 
 if __name__ == '__main__':
 
-    import sys
     sys.setrecursionlimit(RECURSION_LIMIT)
 
     # this if script too slow and profile it
