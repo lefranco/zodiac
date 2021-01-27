75e2ab21aeb5e38af558e6ea8f4cfeee0906e2e7
Jérémie LEFRANCOIS <jlefrancois-ext@gaci.fr>
2021-01-27 17:11:59 +0100

    .

--- sampler.py
+++ sampler.py
@@ -17,14 +17,14 @@ ALPHABET = [chr(i) for i in range(ord('a'), ord('z') + 1)]
 
 
 class Plain:
-    """ A plain : basically a string """
+    """ A plain : list of words and list of letters """
 
     def __init__(self, filename: str) -> None:
 
-        self._content: typing.List[str] = list()
+        self._letters: typing.List[str] = list()
         self._words: typing.List[str] = list()
 
-        with open(filename) as filepointer:
+        with open(filename, encoding='utf-8') as filepointer:
             for line in filepointer:
                 line = line.rstrip('\n')
                 if line:
@@ -32,29 +32,37 @@ class Plain:
                     only_ascii = nfkd_form.encode('ASCII', 'ignore')
                     only_ascii_str = only_ascii.decode()
                     for word in only_ascii_str.split():
-                        word = word.lower()
-                        word = ''.join([ll for ll in word if ll in ALPHABET])
+                        word = word.upper()
+                        word = ''.join([ll for ll in word if ll.lower() in ALPHABET])
                         if word:
                             self._words.append(word)
                             for letter in word:
-                                assert letter in ALPHABET
-                                self._content.append(letter)
+                                if letter.lower() in ALPHABET:
+                                    self._letters.append(letter)
 
-        self._plain_str = ''.join(self._content)
+        self._plain_str = ''.join(self._letters)
 
-    def stats(self, file_handle: typing.TextIO) -> None:
+    def stats_ngrams(self, file_handle: typing.TextIO, n_value: int) -> None:
         """ stats """
 
-        # counting letters
-        quads_count = collections.Counter(self._words)
-
-        # counting  quadgrams
-#        quads_count = collections.Counter([self._plain_str[p: p+4] for p in range(len(self._plain_str)-4)])
+        # counting  ngrams
+        ngrams_count = collections.Counter([self._plain_str[p: p + n_value] for p in range(len(self._plain_str) - n_value + 1)])
 
         with contextlib.redirect_stdout(file_handle):
-            for quad in sorted(quads_count, key=lambda q: quads_count[q], reverse=True):
-                num = quads_count[quad]
-                print(f"{quad} {num}")
+            for ngram in sorted(ngrams_count, key=lambda n: ngrams_count[n], reverse=True):
+                num = ngrams_count[ngram]
+                print(f"{ngram} {num}")
+
+    def stats_dict(self, file_handle: typing.TextIO) -> None:
+        """ stats """
+
+        # counting  ngrams
+        word_count = collections.Counter(self._words)
+
+        with contextlib.redirect_stdout(file_handle):
+            for word in sorted(word_count, key=lambda w: word_count[w], reverse=True):
+                num = word_count[word]
+                print(f"{word} {num}")
 
     def __str__(self) -> str:
         return self._plain_str
@@ -68,17 +76,27 @@ def main() -> None:
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', required=True, help='input file with plain (can have spaces within - will be removed - can have accents - will be corrected)')
-    parser.add_argument('-o', '--output', required=False, help='quads')
+    parser.add_argument('-n', '--n_value', required=False, help='n value for ngrams')
+    parser.add_argument('-g', '--ngrams_output', required=False, help='output ngrams frequency file')
+    parser.add_argument('-d', '--dict_output', required=False, help='output word frequency file')
     args = parser.parse_args()
 
     plain_input_file = args.input
     global PLAIN
     PLAIN = Plain(plain_input_file)
 
-    plain_output_file = args.output
-    if plain_output_file is not None:
-        with open(plain_output_file, 'w') as file_handle:
-            PLAIN.stats(file_handle)
+    ngrams_output_output_file = args.ngrams_output
+    if ngrams_output_output_file is not None:
+        assert args.n_value is not None, "Need a n value for ngrams"
+        n_value = int(args.n_value)
+        assert 1 <= n_value <= 8, "Incorrect value for ngrams"
+        with open(ngrams_output_output_file, 'w') as file_handle:
+            PLAIN.stats_ngrams(file_handle, n_value)
+
+    dict_output_output_file = args.dict_output
+    if dict_output_output_file is not None:
+        with open(dict_output_output_file, 'w') as file_handle:
+            PLAIN.stats_dict(file_handle)
 
 
 if __name__ == '__main__':
