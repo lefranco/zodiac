6ff865b68e9264e933cbf72e93c304672d9e4ab9
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-01-31 11:58:07 +0100

    .

--- ciphers/homophones/poe/poe_cipher.py
+++ ciphers/homophones/poe/poe_cipher.py
@@ -27,36 +27,59 @@ BIG_FONT_SIZE = 40
 SMALL_FONT_SIZE = 30
 
 
-
 class Plain:
     """ A cipher : basically a string """
 
     def __init__(self, filename: str) -> None:
 
-        self._clear_content: typing.List[str] = list()
+        self._content: typing.List[typing.List[str]] = list()
 
         with open(filename) as filepointer:
             for line in filepointer:
                 line = line.rstrip('\n')
-                if line:
-                    nfkd_form = unicodedata.normalize('NFKD', line)
-                    only_ascii = nfkd_form.encode('ASCII', 'ignore')
-                    only_ascii_str = only_ascii.decode()
-                    for word in only_ascii_str.split():
-                        word = word.lower()
-                        for letter in word:
-                            if letter not in ALPHABET:
-                                continue
-                            self._clear_content.append(letter)
-
+                if not line:
+                    continue
+                line_plains: typing.List[str] = list()
+                nfkd_form = unicodedata.normalize('NFKD', line)
+                only_ascii = nfkd_form.encode('ASCII', 'ignore')
+                only_ascii_str = only_ascii.decode()
+                for word in only_ascii_str.split():
+                    word = word.lower()
+                    for letter in word:
+                        if letter not in ALPHABET:
+                            continue
+                        line_plains.append(letter)
+                self._content.append(line_plains)
 
     def display(self, img_page: typing.Any) -> None:
         """ output to old fashion text """
 
-        pass #assert False, "TODO"
+        def display_char(char: str) -> typing.Any:
+            """ output to old fashion text """
+
+            img_char = Image.new('RGB', (CHAR_WIDTH, CHAR_HEIGHT), color="white")
+            draw = ImageDraw.Draw(img_char)
+            font = ImageFont.truetype('/Library/Fonts/FreeSerif.ttf', (BIG_FONT_SIZE + SMALL_FONT_SIZE) // 2)
+            width_font, height_font = draw.textsize(char, font=font)
+
+            pos = ((CHAR_WIDTH - width_font) // 2, (CHAR_HEIGHT - height_font) // 2)
+
+            draw.text(pos, char, fill="red", font=font)
+            return img_char
+
+        cur_pos = (0, CHAR_HEIGHT)
+        for line_plains in self._content:
+            print(''.join(line_plains))
+            for plain in line_plains:
+                img_char = display_char(plain)
+                assert cur_pos[0] + CHAR_WIDTH <= PAGE_WIDTH, "Display horizontal overflow"
+                assert cur_pos[1] + CHAR_HEIGHT <= PAGE_HEIGHT, "Display vertical overflow"
+                img_page.paste(img_char, cur_pos)
+                cur_pos = (cur_pos[0] + CHAR_WIDTH, cur_pos[1])
+            cur_pos = (0, cur_pos[1] + 2 * CHAR_HEIGHT)
 
     def __str__(self) -> str:
-        return ''.join(self._clear_content)
+        return '\n'.join([' '.join([str(c) for c in ll]) for ll in self._content])
 
 
 PLAIN: typing.Optional[Plain] = None
@@ -132,14 +155,13 @@ class Cipher:
                 cur_pos = (cur_pos[0] + CHAR_WIDTH, cur_pos[1])
             cur_pos = (0, cur_pos[1] + 2 * CHAR_HEIGHT)
 
-
     def output(self, file_name: str) -> None:
         """ make cipher for solver """
 
         assert False, "Not implemented"
 
     def __str__(self) -> str:
-        return '\n'.join([' '.join([str(c) for c in l]) for l in self._content])
+        return '\n'.join([' '.join([str(c) for c in ll]) for ll in self._content])
 
 
 CIPHER: typing.Optional[Cipher] = None
