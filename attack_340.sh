151722f36c81e7db84d3b92f209bcd0563c22f5f
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-02-02 18:32:00 +0100

    .

--- attack_340.sh
+++ attack_340.sh
@@ -1,1 +1,1 @@
-sudo nice -n 5 pypy3 ./homophonic.py -p 10 -i ./data/english/raw/ioc.txt -n ./data/english/freq/english_quintgrams.txt -d ./data/english/dict/english_words.txt -l ./data/english/freq/english_monograms.txt -c ./ciphers/homophones/zodiac/cipher-340-transposed-short.txt  -o result.txt
+sudo nice -n 5 pypy3 ./homophonic.py -p 10 -i ./data/english/raw/ioc.txt -n ./data/english/freq/english_quintgrams.txt -d ./data/english/dict/english_words.txt -l ./data/english/freq/english_monograms.txt -c ./ciphers/homophones/zodiac/cipher-340-transposed-short.txt -b -o result.txt
